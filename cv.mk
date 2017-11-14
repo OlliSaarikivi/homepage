e8c601edb21548d22c3f02b235a8165423a8285c
Olli Saarikivi <olli.saarikivi@iki.fi>
2017-11-14 21:44:07 +0200

    Create cv.mk

---
+++ cv.mk
@@ -0,0 +1,7 @@
+---
+title: CV
+---
+
+You can find my CV below. To get it as a PDF please use the menu at the bottom of the embed.
+
+<iframe src="https://onedrive.live.com/embed?cid=2200492C755BF71B&resid=2200492C755BF71B%21191307&authkey=AO9XYa55Ey9iYsY&em=2" width="476" height="288" frameborder="0" scrolling="no"></iframe>
